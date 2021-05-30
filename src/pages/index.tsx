971a182755d7f02e428e89692785afec2c55342e
ayeolakenny <ayeolakenny@gmail.com>
2021-05-30 15:46:32 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index: React.FC<{}> = ({}) => {
+  return (
+    <div>
+      <h1 className="text-blue-900">Testing tailwind</h1>
+    </div>
+  );
+};
+
+export default Index;
